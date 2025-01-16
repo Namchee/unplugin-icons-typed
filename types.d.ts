9ac6f88598ca77c081413f01585801b5a1fb2116
Cristopher <cristophernamchee12@gmail.com>
2025-01-16 21:07:59 +0700

    chore: init project

---
+++ types.d.ts
@@ -0,0 +1,9 @@
+declare module 'virtual:icons/lucide/activity' {
+  const component: string
+  export default component;
+}
+
+declare module '~icons/lucide/activity' {
+  const component: string;
+  export default component;
+}
