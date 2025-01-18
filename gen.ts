4223dc621d8ba54d37b32e88c53ee0fe5929a166
Cristopher <cristophernamchee12@gmail.com>
2025-01-18 22:16:43 +0700

    feat: add auto doc

--- gen.ts
+++ gen.ts
@@ -1,14 +1,15 @@
 import { resolve } from "node:path";
 
-import { readdirSync } from "node:fs";
+import { readdirSync, writeFileSync } from "node:fs";
 import { readFile } from "node:fs/promises";
 
 type IconifyJSON = {
-  icons: Record<string, { body: string }>[];
-  aliases: Record<string, { parent: string }>[];
+  icons: Record<string, { body: string }>;
+  aliases?: Record<string, { parent: string }>;
 }
 
 type IconifyInfo = {
+  prefix: string;
   name: string;
   author: {
     name: string;
@@ -42,29 +43,81 @@ const iconDef: IconPack[] = await Promise.all(
   })
 );
 
-console.log(iconDef);
+const generateIconTypeDeclaration = (icon: string, preview: string, info: IconifyInfo) => `
+declare module 'virtual:icons/${info.prefix}/${icon}' {
+  /**
+   * \`${icon}\` from ${info.name} icon pack
+   * 
+   * ![preview](data:image/svg+xml;base64,${preview})
+   * 
+   * @author ${info.author.name}
+   */
+  const component: string;
+  export default component;
+};
 
-// const generateType = (icon: string) => {
-//   return `
-// declare module 'virtual:icons/lucide/${icon}' {
-//   const component: string;
-//   export default component;
-// }
+declare module '~icons/${info.prefix}/${icon}' {
+  /**
+   * \`${icon}\` from ${info.name} icon pack
+   * 
+   * ![preview](data:image/svg+xml;base64,${preview})
+   * 
+   * @author ${info.author.name}
+   */
+  const component: string;
+  export default component;
+};
+`;
 
-// declare module '~icons/lucide/${icon}' {
-//   const component: string;
-//   export default component;
-// }
-// `;
-// };
+const generateAliasTypeDeclaration = (icon: string, alias: string, info: IconifyInfo) => `
+declare module 'virtual:icons/${info.prefix}/${icon}' {
+  /**
+   * \`${icon}\` from ${info.name} icon pack.
+   * Alias of \`${alias}\`.
+   *
+   * @author ${info.author.name}
+   */
+  const component: string;
+  export default component;
+};
 
-// const type = [
-//   ...new Set([
-//     ...Object.keys(iconDef[0].icons),
-//     ...Object.keys(iconDef[0].aliases),
-//   ]),
-// ].reduce((acc, val) => {
-//   return [acc, generateType(val)].join("");
-// }, "");
+declare module '~icons/${info.prefix}/${icon}' {
+  /**
+   * \`${icon}\` from ${info.name} icon pack.
+   * Alias of \`${alias}\`.
+   *
+   * @author ${info.author.name}
+   */
+  const component: string;
+  export default component;
+};
+`;
 
-// writeFileSync("./.unplugin/type.d.ts", type);
+function generateDeclarationFile(iconPacks: IconPack[]) {
+  let declaration = '';
+
+  for (const pack of iconPacks) {
+    let packDeclaration = `/* ${pack.info.name} icon pack - ${pack.info.author.url} */\n`;
+
+    for (const [key, data] of Object.entries(pack.data.icons)) {
+      const preview = Buffer.from(`<svg xmlns="http://www.w3.org/2000/svg" width="20" height"20">${data.body}</svg>`).toString("base64");
+
+      packDeclaration += generateIconTypeDeclaration(key, preview, pack.info);
+    }
+
+    if (pack.data.aliases) {
+      for (const [key, data] of Object.entries(pack.data.aliases)) {
+        packDeclaration += generateAliasTypeDeclaration(key, data.parent, pack.info);
+      }
+    }
+
+    packDeclaration += `/* End of ${pack.info.name} */\n`;
+
+    declaration += packDeclaration;
+  }
+
+  return declaration;
+}
+
+const dts = generateDeclarationFile(iconDef);
+writeFileSync("./.unplugin/type.d.ts", dts);
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
 
-import A from '~icons/lucide/align-start-horizontal';
+import A from '~icons/lucide/circle';
 
 import { Circle } from 'lucide-react';
 
